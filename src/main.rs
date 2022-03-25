ea7877b53f6a067efd3a9fbe3b1380762c6d9040
xEgoist <egoist@egoistic.dev>
2022-03-25 13:05:42 -0500

    used reqwest instead of ureq for non-blocking requests while ureq is a very minimal http request library, it only implements blocking API. this change requires using tokio for the async function.

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ mod uchecker;
 
 use std::env;
 use uchecker::update_check;
-
-fn main() {
+#[tokio::main]
+async fn main() {
     env_logger::init();
     prompt::handle_ctrlc();
-    let _ = update_check();
+    let _ = update_check().await;
     let args_vec: Vec<String> = env::args().collect();
     let args = args::parse_args(&args_vec[1..]);
 
--- src/uchecker.rs
+++ src/uchecker.rs
@@ -1,16 +1,25 @@
 use ansi_term::Color::{Green, Red};
 use serde::Deserialize;
+
+static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
+
 #[derive(Deserialize)]
 struct Answer {
     tag_name: String,
 }
 
-pub fn update_check() -> Result<(), ureq::Error> {
-    let answer: Answer = ureq::get("https://api.github.com/repos/egoist/dum/releases/latest")
-        .call()?
-        .into_json()?;
-    let version = answer.tag_name.replace("v", "");
-    if env!("CARGO_PKG_VERSION") != version {
+pub async fn update_check() -> Result<(), reqwest::Error> {
+    let client = reqwest::Client::builder()
+        .user_agent(APP_USER_AGENT)
+        .build()?;
+    let answer = client
+        .get("https://api.github.com/repos/egoist/dum/releases/latest")
+        .send()
+        .await?
+        .json::<Answer>()
+        .await?;
+    let version = answer.tag_name.replace('v', "");
+    if env!("CARGO_PKG_VERSION") == version {
         println!(
             "{} {}",
             Red.normal().paint("There is a new version of dum:"),
